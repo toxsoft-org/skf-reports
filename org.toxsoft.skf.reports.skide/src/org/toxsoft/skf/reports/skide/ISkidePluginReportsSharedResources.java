575456ba630edcf5560a4ea992b075fb05c95469
pme <pme@toxsoft.ru>
2023-06-20 16:35:45 +0300

    локализация reports skide

--- org.toxsoft.skf.reports.skide/src/org/toxsoft/skf/reports/skide/ISkidePluginReportsSharedResources.java
+++ org.toxsoft.skf.reports.skide/src/org/toxsoft/skf/reports/skide/ISkidePluginReportsSharedResources.java
@@ -8,32 +8,32 @@ import org.toxsoft.skf.reports.skide.utils.*;
  *
  * @author Max
  */
-@SuppressWarnings( "javadoc" )
+@SuppressWarnings( value = { "nls", "javadoc" } )
 public interface ISkidePluginReportsSharedResources {
 
   /**
    * {@link SkidePluginReports}
    */
-  String STR_SKIDE_REPORTS_MANAGEMENT   = "Редактор шаблонов отчётов";
-  String STR_SKIDE_REPORTS_MANAGEMENT_D = "Редактор шаблонов отчётов";
+  String STR_SKIDE_REPORTS_MANAGEMENT   = Messages.getString( "STR_SKIDE_REPORTS_MANAGEMENT" );
+  String STR_SKIDE_REPORTS_MANAGEMENT_D = Messages.getString( "STR_SKIDE_REPORTS_MANAGEMENT_D" );
 
   /**
    * {@link SkideUnitPanelReports}
    */
-  String STR_TAB_REPORTS     = "Шаблоны отчётов";
-  String STR_TAB_REPORTS_D   = "Редактор шаблонов отчётов";
-  String STR_TAB_GRAPHS      = "Шаблоны графиков";
-  String STR_TAB_GRAPHS_D    = "Редактор шаблонов графиков";
-  String STR_BUTTON_EXPORT_D = "Экспорт шаблонов";
+  String STR_TAB_REPORTS     = Messages.getString( "STR_TAB_REPORTS" );
+  String STR_TAB_REPORTS_D   = Messages.getString( "STR_TAB_REPORTS_D" );
+  String STR_TAB_GRAPHS      = Messages.getString( "STR_TAB_GRAPHS" );
+  String STR_TAB_GRAPHS_D    = Messages.getString( "STR_TAB_GRAPHS_D" );
+  String STR_BUTTON_EXPORT_D = Messages.getString( "STR_BUTTON_EXPORT_D" );
 
   /**
    * {@link ReportsTemplatesMigrationUtiles}
    */
-  String STR_MODEL_GRAPH              = "Шаблоны графиков";
-  String STR_MODEL_GRAPH_D            = "Шаблоны графиков";
-  String STR_MODEL_REPORT             = "Шаблоны отчётов";
-  String STR_MODEL_REPORT_D           = "Шаблоны отчётов";
-  String STR_EXPORT_TEMPLATE_DIALOG   = "Шаблоны для экспорта ";
-  String STR_EXPORT_TEMPLATE_DIALOG_D = "Выбор шаблонов для экспорта";
-  String STR_EXPORT_COMPLETE_DIALOG   = "Экспорт шаблонов завершён";
+  String STR_MODEL_GRAPH              = Messages.getString( "STR_MODEL_GRAPH" );
+  String STR_MODEL_GRAPH_D            = Messages.getString( "STR_MODEL_GRAPH_D" );
+  String STR_MODEL_REPORT             = Messages.getString( "STR_MODEL_REPORT" );
+  String STR_MODEL_REPORT_D           = Messages.getString( "STR_MODEL_REPORT_D" );
+  String STR_EXPORT_TEMPLATE_DIALOG   = Messages.getString( "STR_EXPORT_TEMPLATE_DIALOG" );
+  String STR_EXPORT_TEMPLATE_DIALOG_D = Messages.getString( "STR_EXPORT_TEMPLATE_DIALOG_D" );
+  String STR_EXPORT_COMPLETE_DIALOG   = Messages.getString( "STR_EXPORT_COMPLETE_DIALOG" );
 }

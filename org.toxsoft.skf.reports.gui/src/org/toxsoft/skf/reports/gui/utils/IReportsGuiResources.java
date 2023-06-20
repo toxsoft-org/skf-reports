0f8b624af2811dc2db99123149efe5d50f2a90ba
pme <pme@toxsoft.ru>
2023-06-20 17:56:23 +0300

    Долокализация reports gui

--- org.toxsoft.skf.reports.gui/src/org/toxsoft/skf/reports/gui/utils/IReportsGuiResources.java
+++ org.toxsoft.skf.reports.gui/src/org/toxsoft/skf/reports/gui/utils/IReportsGuiResources.java
@@ -1,5 +1,7 @@
 package org.toxsoft.skf.reports.gui.utils;
 
+import org.toxsoft.skf.reports.gui.*;
+
 /**
  * Локализуемые ресурсы.
  *
@@ -11,18 +13,18 @@ interface IReportsGuiResources {
   /**
    * {@link IntervalSelectionDialogPanel}
    */
-  String START_STR             = "Время начала";
-  String END_STR               = "Время окончания";
-  String DLG_TITLE_PERIOD_SEL  = "Период времени";
-  String DLG_HEADER_PERIOD_SEL = "Выберите время начала и окончания";
+  String START_STR             = Messages.getString( "START_STR" );
+  String END_STR               = Messages.getString( "END_STR" );
+  String DLG_TITLE_PERIOD_SEL  = Messages.getString( "DLG_TITLE_PERIOD_SEL" );
+  String DLG_HEADER_PERIOD_SEL = Messages.getString( "DLG_HEADER_PERIOD_SEL" );
 
   /**
    * {@link ReportTemplateUtilities}
    */
 
-  String FMT_N_TIME_COLUMN       = "Время: %s";
-  String STR_N_TIME_COLUMN       = "Время";
-  String SUMMARY_FIELD_NAME_STR  = "Итого";
-  String DEFAULT_EMPTY_VALUE_STR = "-";
+  String FMT_N_TIME_COLUMN       = Messages.getString( "FMT_N_TIME_COLUMN" );
+  String STR_N_TIME_COLUMN       = Messages.getString( "STR_N_TIME_COLUMN" );
+  String SUMMARY_FIELD_NAME_STR  = Messages.getString( "SUMMARY_FIELD_NAME_STR" );
+  String DEFAULT_EMPTY_VALUE_STR = Messages.getString( "DEFAULT_EMPTY_VALUE_STR" );
 
 }

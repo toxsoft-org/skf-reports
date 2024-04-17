d920665c1447af67f42b651e316825fd425a4c1d
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2024-04-17 10:19:03 +0300

    added correct method select single Skid

--- org.toxsoft.skf.reports.gui/src/org/toxsoft/skf/reports/gui/panels/valed/IReportsGuiResources.java
+++ org.toxsoft.skf.reports.gui/src/org/toxsoft/skf/reports/gui/panels/valed/IReportsGuiResources.java
@@ -9,8 +9,10 @@ interface IReportsGuiResources {
 
   String MSG_ERR_INV_GWID_FORMAT = Messages.getString( "MSG_ERR_INV_GWID_FORMAT" ); //$NON-NLS-1$
   String MSG_ERR_INV_SKID_FORMAT = Messages.getString( "MSG_ERR_INV_SkID_FORMAT" ); //$NON-NLS-1$
+  String DLG_T_SKID_SEL          = Messages.getString("DLG_T_SKID_SEL"); //$NON-NLS-1$
+  String STR_MSG_SKID_SELECTION  = Messages.getString("STR_MSG_SKID_SELECTION"); //$NON-NLS-1$
 
-  String STR_GWID_KIND   = "Тип";
-  String STR_GWID_KIND_D = "Тип GWID'a";
+  String STR_GWID_KIND   = Messages.getString("STR_GWID_KIND"); //$NON-NLS-1$
+  String STR_GWID_KIND_D = Messages.getString("STR_GWID_KIND_D"); //$NON-NLS-1$
 
 }
--- org.toxsoft.skf.reports.gui/src/org/toxsoft/skf/reports/gui/panels/valed/ValedSkidEditor.java
+++ org.toxsoft.skf.reports.gui/src/org/toxsoft/skf/reports/gui/panels/valed/ValedSkidEditor.java
@@ -5,6 +5,11 @@ import static org.toxsoft.core.tslib.av.impl.AvUtils.*;
 import static org.toxsoft.skf.reports.gui.panels.valed.IReportsGuiResources.*;
 
 import org.toxsoft.core.tsgui.bricks.ctx.*;
+import org.toxsoft.core.tsgui.bricks.ctx.impl.*;
+import org.toxsoft.core.tsgui.dialogs.datarec.*;
+import org.toxsoft.core.tsgui.m5.*;
+import org.toxsoft.core.tsgui.m5.gui.*;
+import org.toxsoft.core.tsgui.m5.model.*;
 import org.toxsoft.core.tsgui.valed.api.*;
 import org.toxsoft.core.tsgui.valed.controls.helpers.*;
 import org.toxsoft.core.tsgui.valed.impl.*;
@@ -13,9 +18,10 @@ import org.toxsoft.core.tslib.gw.gwid.*;
 import org.toxsoft.core.tslib.gw.skid.*;
 import org.toxsoft.core.tslib.utils.*;
 import org.toxsoft.core.tslib.utils.errors.*;
-import org.toxsoft.skf.reports.gui.panels.*;
 import org.toxsoft.uskat.core.api.objserv.*;
-import org.toxsoft.uskat.core.api.sysdescr.*;
+import org.toxsoft.uskat.core.connection.*;
+import org.toxsoft.uskat.core.gui.conn.*;
+import org.toxsoft.uskat.core.gui.km5.sded.*;
 
 /**
  * Allows to select {@link Skid} by accessing {@link ISkObjectService}.
@@ -74,13 +80,38 @@ public class ValedSkidEditor
   @Override
   protected boolean doProcessButtonPress() {
     // create and dispaly Skid selector
-    Gwid initVal = canGetValue().isOk() ? Gwid.createObj( getValue() ) : null;
-    Gwid gwid = PanelGwidSelector.selectGwid( initVal, tsContext(), ESkClassPropKind.RTDATA, null );
-    if( gwid != null ) {
-      doSetUnvalidatedValue( gwid.skid() );
+    Skid initVal = canGetValue().isOk() ? Gwid.createObj( getValue() ).skid() : null;
+    // dima 17.04.24
+    // Gwid gwid = PanelGwidSelector.selectGwid( initVal, tsContext(), ESkClassPropKind.RTDATA, null );
+    Skid selSkid = selectSkid( initVal, tsContext() );
+    if( selSkid != null ) {
+      doSetUnvalidatedValue( selSkid );
       return true;
     }
     return false;
+  }
+
+  /**
+   * Выводит диалог выбора Skid.
+   * <p>
+   *
+   * @param aInitSkid {@link Skid} для инициализации
+   * @param aContext {@link ITsGuiContext} - контекст
+   * @return Skid - выбранный объект или <b>null</b> в случает отказа от выбора
+   */
+  Skid selectSkid( Skid aInitSkid, ITsGuiContext aContext ) {
+    TsNullArgumentRtException.checkNull( aContext );
+    ISkConnectionSupplier connSupplier = aContext.get( ISkConnectionSupplier.class );
+    ISkConnection conn = connSupplier.defConn();
+    IM5Domain m5 = conn.scope().get( IM5Domain.class );
+
+    IM5Model<ISkObject> modelSk = m5.getModel( IKM5SdedConstants.MID_SDED_SK_OBJECT, ISkObject.class );
+    IM5LifecycleManager<ISkObject> lmSk = modelSk.getLifecycleManager( conn );
+    ITsGuiContext ctx = new TsGuiContext( aContext );
+    TsDialogInfo di = new TsDialogInfo( ctx, DLG_T_SKID_SEL, STR_MSG_SKID_SELECTION );
+    ISkObject initObj = aInitSkid == null ? null : conn.coreApi().objService().get( aInitSkid );
+    ISkObject selObj = M5GuiUtils.askSelectItem( di, modelSk, initObj, lmSk.itemsProvider(), lmSk );
+    return selObj.skid();
   }
 
   @Override

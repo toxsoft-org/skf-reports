42f2cadeee6ad8565a2ab65537040e00fc8da4de
Yoshimuto <tdo@toxsoft.ru>
2024-12-05 18:14:33 +0400

    L10n

--- org.toxsoft.skf.reports.skide/src/org/toxsoft/skf/reports/skide/ISkidePluginReportsSharedResources.java
+++ org.toxsoft.skf.reports.skide/src/org/toxsoft/skf/reports/skide/ISkidePluginReportsSharedResources.java
@@ -14,28 +14,28 @@ public interface ISkidePluginReportsSharedResources {
   /**
    * {@link SkidePluginReports}
    */
-  String STR_SKIDE_REPORTS_MANAGEMENT   = Messages.getString( "STR_SKIDE_REPORTS_MANAGEMENT" );
-  String STR_SKIDE_REPORTS_MANAGEMENT_D = Messages.getString( "STR_SKIDE_REPORTS_MANAGEMENT_D" );
+  String STR_SKIDE_REPORTS_MANAGEMENT   = Messages.getString( "STR_SKIDE_REPORTS_MANAGEMENT" );   //$NON-NLS-1$
+  String STR_SKIDE_REPORTS_MANAGEMENT_D = Messages.getString( "STR_SKIDE_REPORTS_MANAGEMENT_D" ); //$NON-NLS-1$
 
   /**
    * {@link SkideUnitPanelReports}
    */
-  String STR_TAB_REPORTS        = Messages.getString( "STR_TAB_REPORTS" );
-  String STR_TAB_REPORTS_D      = Messages.getString( "STR_TAB_REPORTS_D" );
-  String STR_TAB_GRAPHS         = Messages.getString( "STR_TAB_GRAPHS" );
-  String STR_TAB_GRAPHS_D       = Messages.getString( "STR_TAB_GRAPHS_D" );
-  String STR_BUTTON_EXPORT_D    = Messages.getString( "STR_BUTTON_EXPORT_D" );
-  String STR_TAB_SPEC_REPORTS_D = Messages.getString( "STR_TAB_SPEC_REPORTS_D" );
-  String STR_TAB_SPEC_REPORTS   = Messages.getString( "STR_TAB_SPEC_REPORTS" );
+  String STR_TAB_REPORTS        = Messages.getString( "STR_TAB_REPORTS" );        //$NON-NLS-1$
+  String STR_TAB_REPORTS_D      = Messages.getString( "STR_TAB_REPORTS_D" );      //$NON-NLS-1$
+  String STR_TAB_GRAPHS         = Messages.getString( "STR_TAB_GRAPHS" );         //$NON-NLS-1$
+  String STR_TAB_GRAPHS_D       = Messages.getString( "STR_TAB_GRAPHS_D" );       //$NON-NLS-1$
+  String STR_BUTTON_EXPORT_D    = Messages.getString( "STR_BUTTON_EXPORT_D" );    //$NON-NLS-1$
+  String STR_TAB_SPEC_REPORTS_D = Messages.getString( "STR_TAB_SPEC_REPORTS_D" ); //$NON-NLS-1$
+  String STR_TAB_SPEC_REPORTS   = Messages.getString( "STR_TAB_SPEC_REPORTS" );   //$NON-NLS-1$
 
   /**
    * {@link ReportsTemplatesMigrationUtiles}
    */
-  String STR_MODEL_GRAPH              = Messages.getString( "STR_MODEL_GRAPH" );
-  String STR_MODEL_GRAPH_D            = Messages.getString( "STR_MODEL_GRAPH_D" );
-  String STR_MODEL_REPORT             = Messages.getString( "STR_MODEL_REPORT" );
-  String STR_MODEL_REPORT_D           = Messages.getString( "STR_MODEL_REPORT_D" );
-  String STR_EXPORT_TEMPLATE_DIALOG   = Messages.getString( "STR_EXPORT_TEMPLATE_DIALOG" );
-  String STR_EXPORT_TEMPLATE_DIALOG_D = Messages.getString( "STR_EXPORT_TEMPLATE_DIALOG_D" );
-  String STR_EXPORT_COMPLETE_DIALOG   = Messages.getString( "STR_EXPORT_COMPLETE_DIALOG" );
+  String STR_MODEL_GRAPH              = Messages.getString( "STR_MODEL_GRAPH" );              //$NON-NLS-1$
+  String STR_MODEL_GRAPH_D            = Messages.getString( "STR_MODEL_GRAPH_D" );            //$NON-NLS-1$
+  String STR_MODEL_REPORT             = Messages.getString( "STR_MODEL_REPORT" );             //$NON-NLS-1$
+  String STR_MODEL_REPORT_D           = Messages.getString( "STR_MODEL_REPORT_D" );           //$NON-NLS-1$
+  String STR_EXPORT_TEMPLATE_DIALOG   = Messages.getString( "STR_EXPORT_TEMPLATE_DIALOG" );   //$NON-NLS-1$
+  String STR_EXPORT_TEMPLATE_DIALOG_D = Messages.getString( "STR_EXPORT_TEMPLATE_DIALOG_D" ); //$NON-NLS-1$
+  String STR_EXPORT_COMPLETE_DIALOG   = Messages.getString( "STR_EXPORT_COMPLETE_DIALOG" );   //$NON-NLS-1$
 }

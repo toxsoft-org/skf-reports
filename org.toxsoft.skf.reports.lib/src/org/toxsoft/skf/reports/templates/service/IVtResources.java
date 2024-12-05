6247500966acb1786c406c0757e3667ced927ab1
Yoshimuto <tdo@toxsoft.ru>
2024-12-05 18:13:00 +0400

    L10n

--- org.toxsoft.skf.reports.lib/src/org/toxsoft/skf/reports/templates/service/IVtResources.java
+++ org.toxsoft.skf.reports.lib/src/org/toxsoft/skf/reports/templates/service/IVtResources.java
@@ -13,50 +13,50 @@ public interface IVtResources {
   /**
    * {@link VtReportTemplateService}
    */
-  String FMT_ERR_NOT_REPORT_TEMPLATE_DPU = Messages.getString( "FMT_ERR_NOT_REPORT_TEMPLATE_DPU" );
-  String MSG_ERR_NO_PARAMS               = Messages.getString( "MSG_ERR_NO_PARAMS" );
+  String FMT_ERR_NOT_REPORT_TEMPLATE_DPU = Messages.getString( "FMT_ERR_NOT_REPORT_TEMPLATE_DPU" ); //$NON-NLS-1$
+  String MSG_ERR_NO_PARAMS               = Messages.getString( "MSG_ERR_NO_PARAMS" );               //$NON-NLS-1$
 
   /**
    * {@link EAggregationFunc}
    */
-  String STR_N_AVERAGE = Messages.getString( "STR_N_AVERAGE" );
-  String STR_D_AVERAGE = Messages.getString( "STR_D_AVERAGE" );
-  String STR_N_MIN     = Messages.getString( "STR_N_MIN" );
-  String STR_D_MIN     = Messages.getString( "STR_D_MIN" );
-  String STR_N_MAX     = Messages.getString( "STR_N_MAX" );
-  String STR_D_MAX     = Messages.getString( "STR_D_MAX" );
-  String STR_N_SUM     = Messages.getString( "STR_N_SUM" );
-  String STR_D_SUM     = Messages.getString( "STR_D_SUM" );
-  String STR_N_COUNT   = Messages.getString( "STR_N_COUNT" );
-  String STR_D_COUNT   = Messages.getString( "STR_D_COUNT" );
+  String STR_N_AVERAGE = Messages.getString( "STR_N_AVERAGE" ); //$NON-NLS-1$
+  String STR_D_AVERAGE = Messages.getString( "STR_D_AVERAGE" ); //$NON-NLS-1$
+  String STR_N_MIN     = Messages.getString( "STR_N_MIN" );     //$NON-NLS-1$
+  String STR_D_MIN     = Messages.getString( "STR_D_MIN" );     //$NON-NLS-1$
+  String STR_N_MAX     = Messages.getString( "STR_N_MAX" );     //$NON-NLS-1$
+  String STR_D_MAX     = Messages.getString( "STR_D_MAX" );     //$NON-NLS-1$
+  String STR_N_SUM     = Messages.getString( "STR_N_SUM" );     //$NON-NLS-1$
+  String STR_D_SUM     = Messages.getString( "STR_D_SUM" );     //$NON-NLS-1$
+  String STR_N_COUNT   = Messages.getString( "STR_N_COUNT" );   //$NON-NLS-1$
+  String STR_D_COUNT   = Messages.getString( "STR_D_COUNT" );   //$NON-NLS-1$
 
   /**
    * {@link IVtTemplateEditorServiceHardConstants}
    */
-  String STR_N_AGGR_FUNC           = Messages.getString( "STR_N_AGGR_FUNC" );
-  String STR_D_AGGR_FUNC           = Messages.getString( "STR_D_AGGR_FUNC" );
-  String STR_N_DISPLAY_FORMAT      = Messages.getString( "STR_N_DISPLAY_FORMAT" );
-  String STR_D_DISPLAY_FORMAT      = Messages.getString( "STR_D_DISPLAY_FORMAT" );
-  String STR_N_TITLE               = Messages.getString( "STR_N_TITLE" );
-  String STR_D_TITLE               = Messages.getString( "STR_D_TITLE" );
-  String STR_N_TEMPLATE_PARAMS     = Messages.getString( "STR_N_TEMPLATE_PARAMS" );
-  String STR_D_TEMPLATE_PARAMS     = Messages.getString( "STR_D_TEMPLATE_PARAMS" );
-  String STR_N_HAS_SUMMARY         = Messages.getString( "STR_N_HAS_SUMMARY" );
-  String STR_D_HAS_SUMMARY         = Messages.getString( "STR_D_HAS_SUMMARY" );
-  String STR_N_AGGR_STEP           = Messages.getString( "STR_N_AGGR_STEP" );
-  String STR_D_AGGR_STEP           = Messages.getString( "STR_D_AGGR_STEP" );
-  String STR_N_CLB_TEMPLATE_PARAMS = Messages.getString( "STR_N_CLB_TEMPLATE_PARAMS" );
-  String STR_D_CLB_TEMPLATE_PARAMS = Messages.getString( "STR_D_CLB_TEMPLATE_PARAMS" );
-  String STR_N_TEMPLATE_AUTHOR     = Messages.getString( "STR_N_TEMPLATE_AUTHOR" );
-  String STR_D_TEMPLATE_AUTHOR     = Messages.getString( "STR_D_TEMPLATE_AUTHOR" );
-  String STR_N_MAX_EXECUTION_TIME  = Messages.getString( "STR_N_MAX_EXECUTION_TIME" );
-  String STR_D_MAX_EXECUTION_TIME  = Messages.getString( "STR_D_MAX_EXECUTION_TIME" );
+  String STR_N_AGGR_FUNC           = Messages.getString( "STR_N_AGGR_FUNC" );           //$NON-NLS-1$
+  String STR_D_AGGR_FUNC           = Messages.getString( "STR_D_AGGR_FUNC" );           //$NON-NLS-1$
+  String STR_N_DISPLAY_FORMAT      = Messages.getString( "STR_N_DISPLAY_FORMAT" );      //$NON-NLS-1$
+  String STR_D_DISPLAY_FORMAT      = Messages.getString( "STR_D_DISPLAY_FORMAT" );      //$NON-NLS-1$
+  String STR_N_TITLE               = Messages.getString( "STR_N_TITLE" );               //$NON-NLS-1$
+  String STR_D_TITLE               = Messages.getString( "STR_D_TITLE" );               //$NON-NLS-1$
+  String STR_N_TEMPLATE_PARAMS     = Messages.getString( "STR_N_TEMPLATE_PARAMS" );     //$NON-NLS-1$
+  String STR_D_TEMPLATE_PARAMS     = Messages.getString( "STR_D_TEMPLATE_PARAMS" );     //$NON-NLS-1$
+  String STR_N_HAS_SUMMARY         = Messages.getString( "STR_N_HAS_SUMMARY" );         //$NON-NLS-1$
+  String STR_D_HAS_SUMMARY         = Messages.getString( "STR_D_HAS_SUMMARY" );         //$NON-NLS-1$
+  String STR_N_AGGR_STEP           = Messages.getString( "STR_N_AGGR_STEP" );           //$NON-NLS-1$
+  String STR_D_AGGR_STEP           = Messages.getString( "STR_D_AGGR_STEP" );           //$NON-NLS-1$
+  String STR_N_CLB_TEMPLATE_PARAMS = Messages.getString( "STR_N_CLB_TEMPLATE_PARAMS" ); //$NON-NLS-1$
+  String STR_D_CLB_TEMPLATE_PARAMS = Messages.getString( "STR_D_CLB_TEMPLATE_PARAMS" ); //$NON-NLS-1$
+  String STR_N_TEMPLATE_AUTHOR     = Messages.getString( "STR_N_TEMPLATE_AUTHOR" );     //$NON-NLS-1$
+  String STR_D_TEMPLATE_AUTHOR     = Messages.getString( "STR_D_TEMPLATE_AUTHOR" );     //$NON-NLS-1$
+  String STR_N_MAX_EXECUTION_TIME  = Messages.getString( "STR_N_MAX_EXECUTION_TIME" );  //$NON-NLS-1$
+  String STR_D_MAX_EXECUTION_TIME  = Messages.getString( "STR_D_MAX_EXECUTION_TIME" );  //$NON-NLS-1$
 
   /**
    * {@link EJrParamSourceType}
    */
-  String STR_N_DATA           = Messages.getString( "STR_N_DATA" );
-  String STR_N_ATTRIBUTES     = Messages.getString( "STR_N_ATTRIBUTES" );
-  String STR_N_RRI_ATTRIBUTES = Messages.getString( "STR_N_RRI_ATTRIBUTES" );
+  String STR_N_DATA           = Messages.getString( "STR_N_DATA" );           //$NON-NLS-1$
+  String STR_N_ATTRIBUTES     = Messages.getString( "STR_N_ATTRIBUTES" );     //$NON-NLS-1$
+  String STR_N_RRI_ATTRIBUTES = Messages.getString( "STR_N_RRI_ATTRIBUTES" ); //$NON-NLS-1$
 
 }

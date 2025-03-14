891e2193b4e9a4f3db982fe7710fb6511486f135
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2025-03-14 14:10:01 +0300

    fix localization

--- org.toxsoft.skf.reports.gui/src/org/toxsoft/skf/reports/gui/panels/ISkResources.java
+++ org.toxsoft.skf.reports.gui/src/org/toxsoft/skf/reports/gui/panels/ISkResources.java
@@ -10,55 +10,54 @@ interface ISkResources {
   /**
    * {@link PanelGwidSelector}
    */
-  String DLG_GWID_SELECTOR        = Messages.getString( "DLG_GWID_SELECTOR" );        //$NON-NLS-1$
-  String DLG_GWID_SELECTOR_D      = Messages.getString( "DLG_GWID_SELECTOR_D" );      //$NON-NLS-1$
-  String MSG_ERR_NO_OBJ_SELECTED  = Messages.getString( "MSG_ERR_NO_OBJ_SELECTED" );  //$NON-NLS-1$
-  String MSG_ERR_NO_PROP_SELECTED = Messages.getString( "MSG_ERR_NO_DATA_SELECTED" ); //$NON-NLS-1$
-  String STR_N_CLASS_PROP_KIND    = Messages.getString( "STR_N_CLASS_PROP_KIND" );    //$NON-NLS-1$
-  String STR_D_CLASS_PROP_KIND    = Messages.getString( "STR_D_CLASS_PROP_KIND" );    //$NON-NLS-1$
-  String STR_CONN_ID_CHAIN        = Messages.getString( "STR_CONN_ID_CHAIN" );        //$NON-NLS-1$
-  String STR_CONN_ID_CHAIN_D      = Messages.getString( "STR_CONN_ID_CHAIN_D" );      //$NON-NLS-1$
+  String DLG_GWID_SELECTOR        = Messages.getString("DLG_GWID_SELECTOR"); //$NON-NLS-1$
+  String DLG_GWID_SELECTOR_D      = Messages.getString("DLG_GWID_SELECTOR_D"); //$NON-NLS-1$
+  String MSG_ERR_NO_OBJ_SELECTED  = Messages.getString("MSG_ERR_NO_OBJ_SELECTED"); //$NON-NLS-1$
+  String MSG_ERR_NO_PROP_SELECTED = Messages.getString("MSG_ERR_NO_PROP_SELECTED"); //$NON-NLS-1$
+  String STR_N_CLASS_PROP_KIND    = Messages.getString("STR_N_CLASS_PROP_KIND"); //$NON-NLS-1$
+  String STR_D_CLASS_PROP_KIND    = Messages.getString("STR_D_CLASS_PROP_KIND"); //$NON-NLS-1$
+  String STR_CONN_ID_CHAIN        = Messages.getString( "STR_CONN_ID_CHAIN" );          //$NON-NLS-1$
+  String STR_CONN_ID_CHAIN_D      = Messages.getString( "STR_CONN_ID_CHAIN_D" );        //$NON-NLS-1$
 
   /**
    * {@link PanelGwidListSelector}
    */
-  String DLG_T_GWID_LIST_SEL         = Messages.getString( "DLG_T_GWID_LIST_SEL" );         //$NON-NLS-1$
-  String STR_MSG_GWID_LIST_SELECTION = Messages.getString( "STR_MSG_GWID_LIST_SELECTION" ); //$NON-NLS-1$
+  String DLG_T_GWID_LIST_SEL         = Messages.getString("DLG_T_GWID_LIST_SEL"); //$NON-NLS-1$
+  String STR_MSG_GWID_LIST_SELECTION = Messages.getString("STR_MSG_GWID_LIST_SELECTION"); //$NON-NLS-1$
 
   /**
    * {@link RtDataInfoViewerPanel}
    */
-  String STR_N_HIDE_ASYNC = Messages.getString( "STR_N_HIDE_ASYNC" ); //$NON-NLS-1$
-  String STR_D_HIDE_ASYNC = Messages.getString( "STR_D_HIDE_ASYNC" ); //$NON-NLS-1$
+  String STR_N_HIDE_ASYNC = Messages.getString("STR_N_HIDE_ASYNC"); //$NON-NLS-1$
+  String STR_D_HIDE_ASYNC = Messages.getString("STR_D_HIDE_ASYNC"); //$NON-NLS-1$
 
   /**
    * {@link GraphTemplateEditorPanel}
    */
-  String STR_N_BY_USERS           = Messages.getString( "STR_N_BY_USERS" );           //$NON-NLS-1$
-  String STR_D_BY_USERS           = Messages.getString( "STR_D_BY_USERS" );           //$NON-NLS-1$
-  String STR_N_GENERATE_CHART     = Messages.getString( "STR_N_GENERATE_CHART" );     //$NON-NLS-1$
-  String STR_D_GENERATE_CHART     = Messages.getString( "STR_D_GENERATE_CHART" );     //$NON-NLS-1$
-  String STR_EXEC_QUERY_REPORT    = Messages.getString( "STR_EXEC_QUERY_REPORT" );    //$NON-NLS-1$
-  String STR_EXEC_QUERY_FOR_GRAPH = Messages.getString( "STR_EXEC_QUERY_FOR_GRAPH" ); //$NON-NLS-1$
-  String ERR_QUERY_FAILED         = Messages.getString( "ERR_QUERY_FAILED" );         //$NON-NLS-1$
+  String STR_N_BY_USERS        = Messages.getString("STR_N_BY_USERS"); //$NON-NLS-1$
+  String STR_D_BY_USERS        = Messages.getString("STR_D_BY_USERS"); //$NON-NLS-1$
+  String STR_N_GENERATE_CHART  = Messages.getString("STR_N_GENERATE_CHART"); //$NON-NLS-1$
+  String STR_D_GENERATE_CHART  = Messages.getString("STR_D_GENERATE_CHART"); //$NON-NLS-1$
+  String STR_EXEC_QUERY_REPORT = Messages.getString("STR_EXEC_QUERY_REPORT"); //$NON-NLS-1$
+  String ERR_QUERY_FAILED      = Messages.getString("ERR_QUERY_FAILED"); //$NON-NLS-1$
 
   /**
    * {@link ReportTemplateEditorPanel}
    */
-  String STR_N_GENERATE_REPORT = Messages.getString( "STR_N_GENERATE_REPORT" ); //$NON-NLS-1$
-  String STR_D_GENERATE_REPORT = Messages.getString( "STR_D_GENERATE_REPORT" ); //$NON-NLS-1$
-  String STR_N_COPY_TEMPLATE   = Messages.getString( "STR_N_COPY_TEMPLATE" );   //$NON-NLS-1$
-  String STR_D_COPY_TEMPLATE   = Messages.getString( "STR_D_COPY_TEMPLATE" );   //$NON-NLS-1$
-  String AUTHOR_STR            = Messages.getString( "AUTHOR_STR" );            //$NON-NLS-1$
-  String DATE_STR              = Messages.getString( "DATE_STR" );              //$NON-NLS-1$
+  String STR_N_GENERATE_REPORT = Messages.getString("STR_N_GENERATE_REPORT"); //$NON-NLS-1$
+  String STR_D_GENERATE_REPORT = Messages.getString("STR_D_GENERATE_REPORT"); //$NON-NLS-1$
+  String STR_N_COPY_TEMPLATE   = Messages.getString("STR_N_COPY_TEMPLATE"); //$NON-NLS-1$
+  String STR_D_COPY_TEMPLATE   = Messages.getString("STR_D_COPY_TEMPLATE"); //$NON-NLS-1$
+  String AUTHOR_STR            = Messages.getString("AUTHOR_STR"); //$NON-NLS-1$
+  String DATE_STR              = Messages.getString("DATE_STR"); //$NON-NLS-1$
 
   /**
    * {@link SpecReportTemplateEditorPanel}
    */
-  String DLG_MES_PARAMS_VAL_SET       = Messages.getString( "DLG_MES_PARAMS_VAL_SET" );       //$NON-NLS-1$
-  String DLG_T_PARAMS_VAL_SET         = Messages.getString( "DLG_T_PARAMS_VAL_SET" );         //$NON-NLS-1$
-  String STR_N_RRI_SECTION            = Messages.getString( "STR_N_RRI_SECTION" );            //$NON-NLS-1$
-  String STR_D_RRI_SECTION            = Messages.getString( "STR_D_RRI_SECTION" );            //$NON-NLS-1$
-  String DLG_TITLE_CHOOSE_RRI_SECTION = Messages.getString( "DLG_TITLE_CHOOSE_RRI_SECTION" ); //$NON-NLS-1$
-  String DLG_CAP_CHOOSE_RRI_SECTION   = Messages.getString( "DLG_CAP_CHOOSE_RRI_SECTION" );   //$NON-NLS-1$
+  String DLG_MES_PARAMS_VAL_SET       = Messages.getString("DLG_MES_PARAMS_VAL_SET"); //$NON-NLS-1$
+  String DLG_T_PARAMS_VAL_SET         = Messages.getString("DLG_T_PARAMS_VAL_SET"); //$NON-NLS-1$
+  String STR_N_RRI_SECTION            = Messages.getString("STR_N_RRI_SECTION"); //$NON-NLS-1$
+  String STR_D_RRI_SECTION            = Messages.getString("STR_D_RRI_SECTION"); //$NON-NLS-1$
+  String DLG_TITLE_CHOOSE_RRI_SECTION = Messages.getString("DLG_TITLE_CHOOSE_RRI_SECTION"); //$NON-NLS-1$
+  String DLG_CAP_CHOOSE_RRI_SECTION   = Messages.getString("DLG_CAP_CHOOSE_RRI_SECTION"); //$NON-NLS-1$
 }

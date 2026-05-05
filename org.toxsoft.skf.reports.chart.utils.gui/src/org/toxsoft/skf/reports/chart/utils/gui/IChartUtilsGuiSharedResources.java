71ef51f3d872ce96a86314c535599843940e2520
denis <denis@denis-vm>
2026-05-05 17:36:45 +0400

    L10n

--- org.toxsoft.skf.reports.chart.utils.gui/src/org/toxsoft/skf/reports/chart/utils/gui/IChartUtilsGuiSharedResources.java
+++ org.toxsoft.skf.reports.chart.utils.gui/src/org/toxsoft/skf/reports/chart/utils/gui/IChartUtilsGuiSharedResources.java
@@ -18,38 +18,38 @@ public interface IChartUtilsGuiSharedResources {
   // -------------------------------------------------
   // @link ConsoleWindow
 
-  String STR_D_CONTROL_PANEL = Messages.getString( "STR_D_CONTROL_PANEL" );
-  String STR_N_PLOT_ZOOM_IN  = Messages.getString( "STR_N_PLOT_ZOOM_IN" );
-  String STR_D_PLOT_ZOOM_IN  = Messages.getString( "STR_D_PLOT_ZOOM_IN" );
-  String STR_N_PLOT_ZOOM_OUT = Messages.getString( "STR_N_PLOT_ZOOM_OUT" );
-  String STR_D_PLOT_ZOOM_OUT = Messages.getString( "STR_D_PLOT_ZOOM_OUT" );
+  String STR_D_CONTROL_PANEL = Messages.getString( "STR_D_CONTROL_PANEL" );//$NON-NLS-1$
+  String STR_N_PLOT_ZOOM_IN  = Messages.getString( "STR_N_PLOT_ZOOM_IN" ); //$NON-NLS-1$
+  String STR_D_PLOT_ZOOM_IN  = Messages.getString( "STR_D_PLOT_ZOOM_IN" ); //$NON-NLS-1$
+  String STR_N_PLOT_ZOOM_OUT = Messages.getString( "STR_N_PLOT_ZOOM_OUT" );//$NON-NLS-1$
+  String STR_D_PLOT_ZOOM_OUT = Messages.getString( "STR_D_PLOT_ZOOM_OUT" );//$NON-NLS-1$
 
   //
   // ---------------------------------------------
   // @link ChartPanel
 
-  String STR_SCALE_VALUE      = Messages.getString( "STR_SCALE_VALUE" );
-  String STR_PRINT            = Messages.getString( "STR_PRINT" );
-  String STR_N_CONTROL_PANEL  = Messages.getString( "STR_N_CONTROL_PANEL" );
-  String STR_LEGEND           = Messages.getString( "STR_LEGEND" );
-  String STR_Y_SCALE_AUTOTUNE = "автоподстройка";
-  String STR_VIZIER           = Messages.getString( "STR_VIZIER" );
-  String STR_CHOICE           = Messages.getString( "STR_CHOICE" );
-  String STR_DISPLAY_FORWARD  = Messages.getString( "STR_DISPLAY_FORWARD" );
-  String STR_FORWARD          = Messages.getString( "STR_FORWARD" );
-  String STR_BACK             = Messages.getString( "STR_BACK" );
-  String STR_DISPLAY_BACK     = Messages.getString( "STR_DISPLAY_BACK" );
+  String STR_SCALE_VALUE      = Messages.getString( "STR_SCALE_VALUE" );      //$NON-NLS-1$
+  String STR_PRINT            = Messages.getString( "STR_PRINT" );            //$NON-NLS-1$
+  String STR_N_CONTROL_PANEL  = Messages.getString( "STR_N_CONTROL_PANEL" );  //$NON-NLS-1$
+  String STR_LEGEND           = Messages.getString( "STR_LEGEND" );           //$NON-NLS-1$
+  String STR_Y_SCALE_AUTOTUNE = Messages.getString( "STR_Y_SCALE_AUTOTUNE" ); //$NON-NLS-1$
+  String STR_VIZIER           = Messages.getString( "STR_VIZIER" );           //$NON-NLS-1$
+  String STR_CHOICE           = Messages.getString( "STR_CHOICE" );           //$NON-NLS-1$
+  String STR_DISPLAY_FORWARD  = Messages.getString( "STR_DISPLAY_FORWARD" );  //$NON-NLS-1$
+  String STR_FORWARD          = Messages.getString( "STR_FORWARD" );          //$NON-NLS-1$
+  String STR_BACK             = Messages.getString( "STR_BACK" );             //$NON-NLS-1$
+  String STR_DISPLAY_BACK     = Messages.getString( "STR_DISPLAY_BACK" );     //$NON-NLS-1$
 
   //
   // ---------------------------------------------
   // @link PanelPlotSelection
 
   String STR_TITLE_DOUBLE_CLICK_FOR_CHANGE_VISIBILITY =
-      Messages.getString( "STR_TITLE_DOUBLE_CLICK_FOR_CHANGE_VISIBILITY" );
+      Messages.getString( "STR_TITLE_DOUBLE_CLICK_FOR_CHANGE_VISIBILITY" );                        //$NON-NLS-1$
   String STR_CAP_CHART_SELECTION_FOR_VISUALIZATION    =
-      Messages.getString( "STR_CAP_CHART_SELECTION_FOR_VISUALIZATION" );
-  String STR_SHOW_CHARTS                              = Messages.getString( "STR_SHOW_CHARTS" );
-  String STR_HIDED_CHARTS                             = Messages.getString( "STR_HIDED_CHARTS" );
-  String STR_HIDE_CHARTS                              = Messages.getString( "STR_HIDE_CHARTS" );
-  String STR_VISIBLE_CHARTS                           = Messages.getString( "STR_VISIBLE_CHARTS" );
+      Messages.getString( "STR_CAP_CHART_SELECTION_FOR_VISUALIZATION" );                           //$NON-NLS-1$
+  String STR_SHOW_CHARTS                              = Messages.getString( "STR_SHOW_CHARTS" );   //$NON-NLS-1$
+  String STR_HIDED_CHARTS                             = Messages.getString( "STR_HIDED_CHARTS" );  //$NON-NLS-1$
+  String STR_HIDE_CHARTS                              = Messages.getString( "STR_HIDE_CHARTS" );   //$NON-NLS-1$
+  String STR_VISIBLE_CHARTS                           = Messages.getString( "STR_VISIBLE_CHARTS" );//$NON-NLS-1$
 }
